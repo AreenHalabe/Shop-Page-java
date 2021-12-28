001f3a706aeb47ce7af29de6f088f0c68000ac2b
AreenHalabe <93481480+AreenHalabe@users.noreply.github.com>
2021-12-28 16:00:08 +0200

    Add one function

--- Shop page - java/java.js
+++ Shop page - java/java.js
@@ -109,6 +109,15 @@ function showSlid3(n) {
 
 /*end of div num 4.................................................*/
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 
 
 
